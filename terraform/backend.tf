401bc3290166377631cacab98ee3e1dd6be50743
Dante <dnte.jauregui@gmail.com>
2025-06-22 11:14:16 +0200

    adding BackendKey file inside github actions pipeline and adding tfDestroy pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,6 @@
 terraform {
   backend "s3" {
+    # "Key" in this terraform block will only work when you do terraform-apply from your local machine. If run using pipelines, it will be overwritting anyway for its own Key "dev.state" or "prod.state" depending:
     key     = "terraform/state.tfstate"
     encrypt = true
   }
