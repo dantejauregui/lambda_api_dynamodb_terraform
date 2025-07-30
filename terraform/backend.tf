c3a819422d5d64a85b4cf8c199084feb47277b11
Dante <dnte.jauregui@gmail.com>
2025-07-30 12:12:00 +0200

    improving only better naming of s3 bucket for backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # "Key" in this terraform block will only work when you do terraform-apply from your local machine. If run using pipelines, it will be overwritting anyway for its own Key "dev.state" or "prod.state" depending:
-    key     = "terraform/dev.tfstate"
+    key     = "lambda_api_dynamoDB/dev.tfstate"
     encrypt = true
   }
 }
--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -13,7 +13,7 @@ resource "aws_api_gateway_resource" "hello_resource" {
 }
 
 
-# ApiGateway will support GET requests:
+#ApiGateway will support GET requests:
 resource "aws_api_gateway_method" "hello_method" {
   rest_api_id   = aws_api_gateway_rest_api.hello_api.id
   resource_id   = aws_api_gateway_resource.hello_resource.id
@@ -22,7 +22,7 @@ resource "aws_api_gateway_method" "hello_method" {
 }
 
 
-# ApiGateway will support POST requests:
+#ApiGateway will support POST requests:
 resource "aws_api_gateway_method" "hello_method_post" {
   rest_api_id   = aws_api_gateway_rest_api.hello_api.id
   resource_id   = aws_api_gateway_resource.hello_resource.id
@@ -31,14 +31,14 @@ resource "aws_api_gateway_method" "hello_method_post" {
 }
 
 
-# General Integration for Lambda & API Gateway for GETting data from OmDB ("integration_http_method" will be always POST, regardless of whether the client request is GET or POST):
+#General Integration for Lambda & API Gateway for GETting data from OmDB ("integration_http_method" will be always POST, regardless of whether the client request is GET or POST):
 resource "aws_api_gateway_integration" "lambda_integration_get" {
   rest_api_id             = aws_api_gateway_rest_api.hello_api.id
   resource_id             = aws_api_gateway_resource.hello_resource.id
   http_method             = aws_api_gateway_method.hello_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.lambda_invoke_arn
+  uri                     = var.lambda_invoke_arn  # will use the values coming from the Lambda Outputs
 }
 # General Integration for Lambda & API Gateway for POSTing data to DynamoDB:
 resource "aws_api_gateway_integration" "lambda_integration_post" {
@@ -47,7 +47,7 @@ resource "aws_api_gateway_integration" "lambda_integration_post" {
   http_method             = aws_api_gateway_method.hello_method_post.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = var.lambda_invoke_arn
+  uri                     = var.lambda_invoke_arn  # will use the values coming from the Lambda Outputs
 }
 
 
